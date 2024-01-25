dd6edef9f643df532a28230cb34edf65906a860b
makaveli10 <suryanvineet47@gmail.com>
2024-01-25 09:16:42 +0100

    Added a build script for dolphin-2_6-phi-2

--- docker/scripts/build-models.sh
+++ docker/scripts/build-models.sh
@@ -4,4 +4,4 @@ test -f /etc/shinit_v2 && source /etc/shinit_v2
 
 ./build-whisper.sh
 # ./build-mistral.sh
-./build-phi-2.sh
+./build-dolphin-2_6-phi-2.sh
--- docker/scripts/run-whisperfusion.sh
+++ docker/scripts/run-whisperfusion.sh
@@ -6,8 +6,8 @@ cd WhisperFusion
 if [ "$1" != "mistral" ]; then
   exec python3 main.py --phi \
                   --whisper_tensorrt_path /root/whisper_small_en \
-                  --phi_tensorrt_path /root/phi-2 \
-                  --phi_tokenizer_path /root/phi-2
+                  --phi_tensorrt_path /root/dolphin-2_6-phi-2 \
+                  --phi_tokenizer_path /root/dolphin-2_6-phi-2/tokenizer
 else
   exec python3 main.py --mistral \
                   --whisper_tensorrt_path /root/models/whisper_small_en \
