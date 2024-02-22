068b55beba57f77058ff10dfb2aca5c47c2d2489
makaveli10 <vineet.suryan@collabora.com>
2024-02-22 05:08:56 -0500

    wait for build-model service to finish before starting whisperfusion service

--- docker/scripts/run-whisperfusion.sh
+++ docker/scripts/run-whisperfusion.sh
@@ -2,15 +2,20 @@
 
 test -f /etc/shinit_v2 && source /etc/shinit_v2
 
+while [ ! -f "/root/scratch-space/models/done.flag" ]; do
+  echo "Waiting for build-models to complete..."
+  sleep 10
+done
+
 cd WhisperFusion
 if [ "$1" != "mistral" ]; then
   exec python3 main.py --phi \
-                  --whisper_tensorrt_path /root/whisper_small_en \
-                  --phi_tensorrt_path /root/dolphin-2_6-phi-2 \
-                  --phi_tokenizer_path /root/dolphin-2_6-phi-2/tokenizer
+                  --whisper_tensorrt_path /root/scratch-space/models/whisper_small_en \
+                  --phi_tensorrt_path /root/scratch-space/models/dolphin-2_6-phi-2 \
+                  --phi_tokenizer_path /root/scratch-space/models/dolphin-2_6-phi-2/tokenizer
 else
   exec python3 main.py --mistral \
-                  --whisper_tensorrt_path /root/models/whisper_small_en \
-                  --mistral_tensorrt_path /root/models/mistral \
+                  --whisper_tensorrt_path /root/scratch-space/models/whisper_small_en \
+                  --mistral_tensorrt_path /root/scratch-space/models/mistral \
                   --mistral_tokenizer_path teknium/OpenHermes-2.5-Mistral-7B
 fi
