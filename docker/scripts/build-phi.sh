33fab605677383dd6f78bb7ffce2824eb6c27beb
makaveli10 <vineet.suryan@collabora.com>
2024-06-13 03:09:05 -0400

    Upgrade TensorRT-LLM v0.10.0 & integrate phi-3
    Signed-off-by: makaveli10 <vineet.suryan@collabora.com>

---
+++ docker/scripts/build-phi.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -e
+
+## Note: Phi is only available in main branch and hasnt been released yet. So, make sure to build TensorRT-LLM from main branch.
+
+cd /root/TensorRT-LLM-examples/phi
+
+## Build TensorRT for Phi-2 with `fp16`
+
+MODEL_TYPE=$1
+echo "Download $MODEL_TYPE Huggingface models..."
+
+phi_path=$(huggingface-cli download --repo-type model microsoft/$MODEL_TYPE)
+echo "Building  TensorRT Engine..."
+name=$1
+pip install -r requirements.txt
+
+python3 ./convert_checkpoint.py --model_type $MODEL_TYPE \
+                    --model_dir $phi_path \
+                    --output_dir ./phi-checkpoint \
+                    --dtype float16
+
+trtllm-build \
+    --checkpoint_dir ./phi-checkpoint \
+    --output_dir $name \
+    --gpt_attention_plugin float16 \
+    --context_fmha enable \
+    --gemm_plugin float16 \
+    --max_batch_size 1 \
+    --max_input_len 1024 \
+    --max_output_len 1024 \
+    --tp_size 1 \
+    --pp_size 1
+
+dest=/root/scratch-space/models
+mkdir -p "$dest/$name/tokenizer"
+cp -r "$name" "$dest"
+(cd "$phi_path" && cp config.json tokenizer_config.json vocab.json merges.txt "$dest/$name/tokenizer")
+cp -r "$phi_path" "$dest/phi-orig-model"
--- docker/scripts/setup-whisperfusion.sh
+++ docker/scripts/setup-whisperfusion.sh
@@ -1,20 +1,17 @@
 #!/bin/bash -e
 
 ## Clone this repo and install requirements
-[ -d "WhisperFusion" ] || git clone -b optimize_docker https://github.com/makaveli10/WhisperFusion.git
+[ -d "WhisperFusion" ] || git clone -b upgrade/tensorrt_llm_0_10_0 https://github.com/makaveli10/WhisperFusion.git
 
 cd WhisperFusion
 apt update
 apt install ffmpeg portaudio19-dev -y
 
-## Install torchaudio matching the PyTorch from the base image
-pip install --extra-index-url https://download.pytorch.org/whl/cu121 torchaudio==2.1.0
-
 ## Install all the other dependencies normally
 pip install -r requirements.txt
 
 ## force update huggingface_hub (tokenizers 0.14.1 spuriously require and ancient <=0.18 version)
-pip install -U huggingface_hub
+pip install -U huggingface_hub tokenizers
 
 huggingface-cli download collabora/whisperspeech t2s-small-en+pl.model s2a-q4-tiny-en+pl.model
 huggingface-cli download charactr/vocos-encodec-24khz
@@ -24,4 +21,4 @@ curl -L -o /root/.cache/torch/hub/checkpoints/encodec_24khz-d7cc33bc.th https://
 mkdir -p /root/.cache/whisper-live/
 curl -L -o /root/.cache/whisper-live/silero_vad.onnx https://github.com/snakers4/silero-vad/raw/master/files/silero_vad.onnx
 
-python -c 'from transformers.utils.hub import move_cache; move_cache()'
+python3 -c 'from transformers.utils.hub import move_cache; move_cache()'
