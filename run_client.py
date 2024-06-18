1993d76ce30d53eed9e34b820aa8808de9e47e5e
makaveli10 <vineet.suryan@collabora.com>
2024-06-18 12:19:32 -0400

    Remove stale python scripts
    Signed-off-by: makaveli10 <vineet.suryan@collabora.com>

--- run_client.py
+++
@@ -1,7 +0,0 @@
-from whisper_live.client import TranscriptionClient
-
-if __name__ == "__main__":
-    client = TranscriptionClient(
-        "0.0.0.0", "6006", is_multilingual=False, lang="en", translate=False
-    )
-    client()   # uses microphone audio
--- run_trt_server.py
+++
@@ -1,5 +0,0 @@
-from whisper_live.trt_server import TranscriptionServer
-
-if __name__ == "__main__":
-    server = TranscriptionServer()
-    server.run("0.0.0.0", 6006)
